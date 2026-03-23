16415a92e5a5f4ac194b3c875b2aeadf56e55431
Illia Savinov <dresar2404@gmail.com>
2026-03-23 16:17:16 +0100

    fixed tasks/models.py

--- tasks/models.py
+++ tasks/models.py
@@ -13,11 +13,13 @@ class Position(models.Model):
 
 
 class Worker(AbstractUser):
-    position = models.ForeignKey(Position,
-                                 on_delete=models.CASCADE,
-                                 null=True,
-                                 blank=True,
-                                 related_name="workers")
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="workers",
+    )
 
     class Meta:
         verbose_name = "worker"
@@ -28,10 +30,12 @@ class Worker(AbstractUser):
             position = self.position
         else:
             position = "no position"
-        return (f"{self.username} "
-                f"({self.first_name}"
-                f" {self.last_name})"
-                f"{position}")
+        return (
+            f"{self.username} "
+            f"({self.first_name}"
+            f" {self.last_name})"
+            f"{position}"
+        )
 
 
 class Priority(models.IntegerChoices):
@@ -52,22 +56,19 @@ class Task(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     deadline = models.DateTimeField()
-    is_completed = models.BooleanField(default=False,
-                                       db_index=True)
-    priority = models.IntegerField(
-        choices=Priority, default=Priority.MEDIUM)
-    task_type = models.ForeignKey(TaskType,
-                                  on_delete=models.CASCADE,
-                                  related_name="tasks")
-    assignees = models.ManyToManyField(Worker,
-                                       related_name="tasks")
-
+    is_completed = models.BooleanField(default=False, db_index=True)
+    priority = models.IntegerField(choices=Priority, default=Priority.MEDIUM)
+    task_type = models.ForeignKey(
+        TaskType, on_delete=models.CASCADE, related_name="tasks"
+    )
+    assignees = models.ManyToManyField(Worker, related_name="tasks")
 
     class Meta:
         ordering = ["-deadline"]
 
     def __str__(self):
-        return (f"{self.name}"
-                f" ({self.task_type.name}"
-                f" | {self.get_priority_display()})")
-
+        return (
+            f"{self.name}"
+            f" ({self.task_type.name}"
+            f" | {self.get_priority_display()})"
+        )
