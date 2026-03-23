6a1dd1af904989c7e846e73aba93e4a685d8e800
Illia Savinov <dresar2404@gmail.com>
2026-03-23 16:48:48 +0100

    register models to be able to create them from django admin panel

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Worker, Task, TaskType
 
-# Register your models here.
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {"fields": ("position",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Info", {"fields": ("position",)}),
+    )
+
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "deadline", "is_completed", "priority", "task_type")
+    list_filter = ("is_completed", "priority", "task_type")
+    search_fields = ("name", "description")
+    filter_horizontal = ("assignees",)
