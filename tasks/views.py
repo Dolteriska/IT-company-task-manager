878bed5901c4234b7815e9e4ded3566b11e40bfa
Illia Savinov <dresar2404@gmail.com>
2026-03-23 16:18:01 +0100

    add basic index view as a main page

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from .models import Worker, Task
+
+
+def index(request):
+
+    tasks = Task.objects.select_related("task_type").all()
+
+    context = {
+        "tasks": tasks,
+        "segment": "index",
+    }
+
+    return render(request, "pages/custom-index.html", context)
