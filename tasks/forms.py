03a86f9faed377be2900a832b1b7c50146e7de0d
Illia Savinov <dresar2404@gmail.com>
2026-03-24 15:20:13 +0100

    changed WorkerRegistrationForm so it would include username and email to registration

--- tasks/forms.py
+++ tasks/forms.py
@@ -6,6 +6,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class WorkerRegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = UserCreationForm.Meta.fields + ("position",)
+        fields = UserCreationForm.Meta.fields + ("username", "email", )
