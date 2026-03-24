eeacd74c8ef43a9098317d50f332f482101a14a9
Illia Savinov <dresar2404@gmail.com>
2026-03-24 17:34:31 +0100

    changed WorkerRegistrationForm, added WorkerSearch, TaskSearch and TaskTypeSearh forms

--- tasks/forms.py
+++ tasks/forms.py
@@ -2,11 +2,46 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 
-
 User = get_user_model()
 
 class WorkerRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
+
     class Meta(UserCreationForm.Meta):
-        model = User
-        fields = UserCreationForm.Meta.fields + ("username", "email", )
+        model = get_user_model()
+
+        fields = ("username", "email", "first_name", "last_name",)
+
+
+class WorkerSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search by username"}
+        )
+    )
+
+
+class TaskSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search by name"
+                   }
+        )
+    )
+
+class TaskTypeSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search by name"
+                   }
+        )
+    )
