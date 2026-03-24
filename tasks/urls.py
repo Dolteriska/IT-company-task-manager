25d65bec400ba30108fa94e7b689977c738c5db3
Illia Savinov <dresar2404@gmail.com>
2026-03-24 17:35:10 +0100

    add workers/ and tasks/ list views to urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path, include
 from django.contrib.auth.views import LoginView
-from .views import index, register_view
+from .views import (
+    index,
+    register_view,
+    WorkerListView,
+    TaskListView,
+
+)
+
 
 urlpatterns = [
     path("", index, name="index"),
     path("register/", register_view, name="register"),
     path("accounts/login/", LoginView.as_view(template_name="accounts/login.html"), name="login"),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("workers/", WorkerListView.as_view(), name="worker-list"),
+    path("tasks/", TaskListView.as_view(), name="task-list")
 
                ]
 
