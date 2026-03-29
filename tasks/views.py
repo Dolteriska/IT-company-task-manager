5c034c8c11a7ea9f2907c81e70e30317a9ba5b1d
Illia Savinov <dresar2404@gmail.com>
2026-03-29 15:51:37 +0200

    changed redirect from succesful change of a user from worker detail to worker list

--- tasks/views.py
+++ tasks/views.py
@@ -159,7 +159,7 @@ class WorkerUpdateView(LoginRequiredMixin,
 
     def get_success_url(self):
         return reverse_lazy(
-            "tasks:worker-detail", kwargs={"pk": self.object.pk})
+            "tasks:worker-list", kwargs={"pk": self.object.pk})
 
 
 class TaskCreateView(LoginRequiredMixin, generic.CreateView):
