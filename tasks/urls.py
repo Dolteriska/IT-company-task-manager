e21e0a2bd6a1bb2c1d1bb5eba5a941f7bc1c7d5c
Illia Savinov <dresar2404@gmail.com>
2026-03-29 15:00:07 +0200

    minor flake8 fixes over the file. Add all pathes needed

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from django.contrib.auth.views import LoginView
 from .views import (
     index,
     register_view,
+    CustomLoginView,
     WorkerListView,
     WorkerDetailView,
     TaskCreateView,
@@ -12,25 +12,73 @@ from .views import (
     TaskTypeUpdateView,
     TaskTypeDeleteView,
     WorkerUpdateView,
-
+    TaskUpdateView,
+    TaskDeleteView,
+    WorkerCreateView,
+    PositionCreateView,
+    PositionListView,
+    PositionUpdateView,
+    PositionDeleteView, WorkerDeleteView,
 )
 
-
 urlpatterns = [
-    path("", index, name="index"),
-    path("register/", register_view, name="register"),
-    path("accounts/login/", LoginView.as_view(template_name="accounts/login.html"), name="login"),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path("workers/", WorkerListView.as_view(), name="worker-list"),
-    path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
-    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
-    path("tasks/", TaskListView.as_view(), name="task-list"),
-    path("task_types/", TaskTypeListView.as_view(), name="task-type-list"),
-    path("task_types/create/", TaskTypeCreateView.as_view(), name="task-type-create"),
-    path("task_types/<int:pk>/update/", TaskTypeUpdateView.as_view(), name="task-type-update"),
-    path("task_types/<int:pk>/delete/", TaskTypeDeleteView.as_view(), name="task-type-delete"),
-    path("workers/<int:pk>/update/", WorkerUpdateView.as_view(), name="worker-update"),
-
-               ]
+    path("",
+         index, name="index"),
+    path("register/",
+         register_view, name="register"),
+    path(
+        "accounts/login/",
+        CustomLoginView.as_view(template_name="accounts/login.html"),
+        name="login",
+    ),
+    path("accounts/",
+         include("django.contrib.auth.urls")),
+    path("workers/",
+         WorkerListView.as_view(), name="worker-list"),
+    path("workers/<int:pk>/delete/",
+         WorkerDeleteView.as_view(), name="worker-delete"),
+    path("workers/<int:pk>/",
+         WorkerDetailView.as_view(), name="worker-detail"),
+    path("tasks/create/",
+         TaskCreateView.as_view(), name="task-create"),
+    path("tasks/",
+         TaskListView.as_view(), name="task-list"),
+    path("task_types/",
+         TaskTypeListView.as_view(), name="task-type-list"),
+    path("task_types/create/",
+         TaskTypeCreateView.as_view(), name="task-type-create"),
+    path(
+        "task_types/<int:pk>/update/",
+        TaskTypeUpdateView.as_view(),
+        name="task-type-update",
+    ),
+    path(
+        "task_types/<int:pk>/delete/",
+        TaskTypeDeleteView.as_view(),
+        name="task-type-delete",
+    ),
+    path("workers/<int:pk>/update/",
+         WorkerUpdateView.as_view(), name="worker-update"),
+    path("tasks/<int:pk>/update/",
+         TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/",
+         TaskDeleteView.as_view(), name="task-delete"),
+    path("workers/create/",
+         WorkerCreateView.as_view(), name="worker-create"),
+    path("positions/",
+         PositionListView.as_view(), name="position-list"),
+    path("positions/create/",
+         PositionCreateView.as_view(), name="position-create"),
+    path(
+        "positions/<int:pk>/update/",
+        PositionUpdateView.as_view(),
+        name="position-update",
+    ),
+    path(
+        "positions/<int:pk>/delete/",
+        PositionDeleteView.as_view(),
+        name="position-delete",
+    ),
+]
 
 app_name = "tasks"
